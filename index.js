52398283143861cb336792789ccebf8b0836bf6f
mguppy <mguppy34@gmail.com>
2021-07-26 17:45:27 -0500

    switch cases working

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 //Including packages for this application
-const fs = require("fs");
-const inquirer = require("inquirer");
-const cTable = require('console.table');
+// const fs = require("fs");
+// const inquirer = require("inquirer");
+// const cTable = require('console.table');
 
 //Presents the user with options to choose
-const allOptions = [
-    { 
-        type: "list",
-        message: "What would you like to do?",
-        name: "user-option",
-        choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department","Add a Role", "Add an Employee", "Update an Employee Role","Quit"],
-    }
-]
+// const allOptions = [
+//     { 
+//         type: "list",
+//         message: "What would you like to do?",
+//         name: "user-option",
+//         choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department","Add a Role", "Add an Employee", "Update an Employee Role","Quit"],
+//     }
+// ]
 
 // Function to initialize app
-function init() {
-    //Get response from manager and decides which SQL query to run
-    inquirer.prompt(allOptions).then((allOptionsResponses) => {
-        if (allOptionsResponses.useroption == 'View All Departments') {
-            // Post route that returns the select all departments
-            });
-        }
-    });
-};
+// function init() {
+//     //Get response from manager and decides which SQL query to run
+//     inquirer.prompt(allOptions).then((allOptionsResponses) => {
+//         if (allOptionsResponses.useroption == 'View All Departments') {
+//             // Post route that returns the select all departments
+//             });
+//         }
+//     });
+// };
--- server.js
+++ server.js
@@ -28,36 +28,46 @@ const db = mysql.createConnection(
 
 //Presents the user with options to choose
 const allOptions = [
-    { 
+    {
         type: "list",
         message: "What would you like to do?",
         name: "useroption",
-        choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department","Add a Role", "Add an Employee", "Update an Employee Role","Quit"],
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Quit"],
     }
 ]
 
-// function allDepartments() {
-//     // Query database
-//     db.query('SELECT * FROM department', function (err, results) {
-//         console.log(results);
-// }
-
 // Function to initialize app
 function init() {
-    //Get response from manager and decides which SQL query to run
+
+    askQuestion();
+};
+
+function askQuestion() {
     inquirer.prompt(allOptions).then((allOptionsResponses) => {
-        if (allOptionsResponses.useroption === 'View All Departments') {
-            // Post route that returns the select all departments
-            // const sql = `SELECT * FROM department`
-            // db.query(sql, function (err, results) {
-            //     console.table(results);
-            // });
-            db.query('SELECT * FROM department', function (err, results) {
-                console.log(results);
-            });
+        // Switch cases
+        var userSelection = allOptionsResponses.useroption;
+        switch (userSelection) {
+            case "View All Departments":
+                db.query('SELECT * FROM department', function (err, results) {
+                    console.table(results);
+                    askQuestion();
+                });
+                break;
+            case "View All Roles":
+                db.query('SELECT * FROM roles', function (err, results) {
+                    console.table(results);
+                    askQuestion();
+                });
+                break;
+            case "View All Employees":
+                db.query('SELECT * from employee', function (err, results){
+                console.table(results);
+                askQuestion();
+                });
+                break;
         }
     });
-};
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
